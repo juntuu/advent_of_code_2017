b44db738e93279d91dc43c6400ac7682c1ea1bb7
Juho Eerola <juho.eerola@tuni.fi>
2020-12-31 01:57:24 +0200

    Day 13, small optimisation

--- day_13/solution.erl
+++ day_13/solution.erl
@@ -2,26 +2,25 @@
 -mode(compile).
 -export([main/1]).
 
-
 main(_) ->
 	Lines = read_lines(),
-	Severity = severity(Lines),
+	Sorted = lists:sort(fun({_, A}, {_, B}) -> A < B end, Lines),
+	Severity = severity(Sorted),
 	io:format("~p~n", [Severity]),
-	Delay = delay(Lines),
+	true = Severity =:= 1876,
+	Delay = delay(Sorted),
 	io:format("~p~n", [Delay]),
-	true = Delay == 3964778,
+	true = Delay =:= 3964778,
 	ok.
 
-caught([D, R]) ->
-	Cycle = (R - 1) * 2,
-	case D rem Cycle of
-		0 -> D * R;
+caught({D, C}) ->
+	case D rem C of
+		0 -> D * (C div 2 + 1);
 		_ -> 0
 	end.
 
-caught([D, R], T) ->
-	Cycle = (R - 1) * 2,
-	((D + T) rem Cycle) == 0.
+caught({D, C}, T) ->
+	((D + T) rem C) =:= 0.
 
 severity(Xs) ->
 	severity(Xs, 0).
@@ -34,19 +33,26 @@ good_trip([], _) -> true;
 good_trip([X|Xs], T) ->
 	not caught(X, T) and good_trip(Xs, T).
 
-delay(Xs) -> delay(Xs, 0).
+delay([{D,2}|Xs]) ->
+	N = bool_num(caught({D,2}, 0)),
+	delay(Xs, N, 2);
+delay(Xs) ->
+	delay(Xs, 0, 1).
 
-delay(Xs, N) ->
+delay(Xs, N, Incr) ->
 	case good_trip(Xs, N) of
 		true -> N;
-		false -> delay(Xs, N+1)
+		false -> delay(Xs, N+Incr, Incr)
 	end.
 
+bool_num(true) -> 1;
+bool_num(false) -> 0.
+
 read_lines() ->
-	lists:reverse(read_lines([])).
+	read_lines([]).
 
 read_lines(X) ->
 	case io:fread("", "~d: ~d") of
-		{ok, Y} -> read_lines([Y|X]);
+		{ok, [D, R]} -> read_lines([{D,(R-1)*2}|X]);
 		_ -> X
 	end.
